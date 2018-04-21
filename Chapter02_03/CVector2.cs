acc55fb3a9080a231b7a013d3a1ee5164e25937d
Hayoung Jeon <wjsgkdud9000@naver.com>
2018-04-21 20:59:22 +0900

    Patial Test

---
+++ Chapter02_03/CVector2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter02_03
+{
+    class CVector2
+    {
+        private int theX;
+       public int theY;
+        public CVector2()
+        {
+            theX = 0;
+            theY = 0;
+        }
+        public int x
+        {
+            get
+            {
+                return theX;
+            }
+            set
+            {
+                theX = value;
+            }
+        }
+        public int Y
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Chapter02_03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter02_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CPartialClass tmpPc = new CPartialClass();
+            Console.WriteLine(tmpPc.theModule1);
+            Console.WriteLine(tmpPc.theModule2);
+            Console.WriteLine(tmpPc.FunctionA());
+            Console.WriteLine(tmpPc.FunctionB());
+
+            CVector2 tmpV2 = new CVector2();
+            tmpV2.x = 3;
+            Console.WriteLine(tmpV2.x);
+            tmpV2.Y = 7;
+            Console.WriteLine(tmpV2.Y);
+
+            var tmpStudent = new { Number = 1, Name = "Mirim" };
+            Console.WriteLine(tmpStudent.Number);
+            Console.WriteLine(tmpStudent.Name);
+        }
+       partial class CPartialClass
+        {
+            public CPartialClass()
+            {
+                theModule1 = 5;
+                theModule2 = 3;
+            }
+            public int theModule2;
+            public int FunctionB()
+            {
+                return 2;
+            }
+        }
+       
+    }
+}
